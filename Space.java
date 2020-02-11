e3915ca4b9f45316be27927a5cece19d1bfb654e
HenryCollins14 <60672325+HenryCollins14@users.noreply.github.com>
2020-02-11 12:38:00 -0800

    Completed Lab 1

--- Space.java
+++ Space.java
@@ -25,6 +25,7 @@ public class Space extends World
         addObject(rocket, getWidth()/2 + 100, getHeight()/2);
         
         addAsteroids(startAsteroids);
+        paintStars(300);
         
         scoreCounter = new Counter("Score: ");
         addObject(scoreCounter, 60, 480);
@@ -44,6 +45,20 @@ public class Space extends World
             int x = Greenfoot.getRandomNumber(getWidth()/2);
             int y = Greenfoot.getRandomNumber(getHeight()/2);
             addObject(new Asteroid(), x, y);
+        }
+    }
+    
+     private void paintStars(int count) 
+    {
+        GreenfootImage background = getBackground();
+        for(int i = 0; i < count; i++) 
+        {
+            int x = Greenfoot.getRandomNumber(getWidth());
+            int y = Greenfoot.getRandomNumber(getHeight());
+            int size = Greenfoot.getRandomNumber(5);
+            int a = Greenfoot.getRandomNumber(155);
+            background.setColor(new Color(a + 40, a , a));
+            background.fillOval(x, y, size, size);
         }
     }
     
